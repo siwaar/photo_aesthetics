439e3ab4ff3183d2bf62baddf0bb8a93002db833
ABBES Siwar (Consulting For ACCOR Hotels) <siwar.abbes@consulting-for.accor.com>
2023-06-26 23:07:42 +0200

    model

--- test.py
+++ test.py
@@ -39,12 +39,9 @@ model = model.to(device)
 model.eval()
 
 test_transform = transforms.Compose([
-    transforms.Resize((256,256)), 
-    #transforms.RandomCrop(224), 
-    transforms.CenterCrop(224), 
-    transforms.ToTensor(), 
-    transforms.Normalize(mean=[0.485, 0.456, 0.406], 
-                         std=[0.229, 0.224, 0.225])
+    transforms.Scale(256), 
+    transforms.RandomCrop(224), 
+    transforms.ToTensor()
     ])
 
 testing_imgs  = args.test_images
